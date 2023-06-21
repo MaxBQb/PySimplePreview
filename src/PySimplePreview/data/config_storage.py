b15b511229da1ea074c9ef066bcfb8b0b85db950
Бабышев Максим <cmdazaza@gmail.com>
2023-06-21 13:52:09 +0300

    Preview window settings now persistent

--- src/PySimplePreview/data/config_storage.py
+++ src/PySimplePreview/data/config_storage.py
@@ -1,14 +1,32 @@
+import jsons
+
 from src.PySimplePreview.domain.model.config import Config
 
 
 class ConfigStorage:
     _instance = None
 
-    def __init__(self):
-        self.config = Config()
+    def __init__(self, filename: str = 'config.json'):
+        self._config = None
+        self.filename = filename
+
+    @property
+    def config(self):
+        if self._config is None:
+            self.load()
+        return self._config
+
+    def load(self):
+        try:
+            self._config = Config()
+            with open(self.filename, 'r', encoding='utf-8-sig') as file:
+                self._config = jsons.loads(file.read(), Config)
+        except (OSError, jsons.DeserializationError):
+            self.save()
 
     def save(self):
-        pass
+        with open(self.filename, 'w', encoding='utf-8-sig') as file:
+            file.write(jsons.dumps(self._config or Config(), jdkwargs=dict(indent=2)))
 
     @classmethod
     def get(cls):
