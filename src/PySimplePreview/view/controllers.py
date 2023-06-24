d6e2777238fbb147014bb295d4b58039dac1df70
Бабышев Максим <cmdazaza@gmail.com>
2023-06-24 22:47:15 +0300

    Preview updates now less flickering

--- src/PySimplePreview/view/controllers.py
+++ src/PySimplePreview/view/controllers.py
@@ -11,11 +11,43 @@ from src.PySimplePreview.view.layouts import get_settings_layout, get_preview_la
 from src.PySimplePreview.view.models import map_config_to_view, ListItem, shorten_preview_names
 
 
+class WindowHolder:
+    def __init__(self):
+        self._window: sg.Window = None
+        self._new_window: sg.Window = None
+
+    @property
+    def window(self):
+        return self._window
+
+    def swap(self, new_window: sg.Window):
+        self._new_window = new_window
+
+    def step(self):
+        if self._new_window:
+            result = self._read(self._new_window, 0)
+            self._new_window.set_alpha(1)
+            self.close()
+            self._window = self._new_window
+            self._new_window = None
+            return result
+        if not self._window or self.window.was_closed():
+            return None
+        return self._read(self._window, 1)
+
+    def _read(self, window, timeout):
+        return window.read(timeout)
+
+    def close(self):
+        if self._window:
+            self.window.close()
+
+
 class PreviewWindowController:
     def __init__(self):
         self._configs_storage = ConfigStorage.get()
         self._previews_storage = PreviewsStorage.get()
-        self._window: sg.Window = None
+        self._window_holder = WindowHolder()
         self.queue = Queue()
 
     @property
@@ -25,12 +57,6 @@ class PreviewWindowController:
     @property
     def _previews(self):
         return self._previews_storage.previews
-
-    def _swap_window(self, value):
-        if self._window:
-            self._window.close()
-            self._window = None
-        self._window = value
 
     def make_layout(self, layout: Callable[[], list[list]]):
         config = map_config_to_view(self._config)
@@ -70,25 +96,28 @@ class PreviewWindowController:
             size=self._config.size,
             resizable=True,
             finalize=True,
+            alpha_channel=0.0,
         )
-        self._swap_window(window)
-        self._window.bind('<Configure>', "Configure")
+        window.bind('<Configure>', "Configure")
+        self._window_holder.swap(window)
 
     def step(self):
         if not self.queue.empty():
             self._set_layout(self.queue.get_nowait())
-        if self._window:
-            try:
-                event, values = self._window.read(1)
+        try:
+            result = self._window_holder.step()
+            if result:
+                event, values = result
                 self._handle_event(event, values)
-            except Exception as e:
-                print(e)
-        else:
+            else:
+                time.sleep(1)
+        except Exception as e:
+            print(e)
             time.sleep(1)
 
     def _handle_event(self, event, values):
         if event is None:
-            self._window = None
+            self._window_holder.close()
         elif event == "theme":
             self._config.theme = values[event]
             self._configs_storage.save()
@@ -98,8 +127,8 @@ class PreviewWindowController:
             self._configs_storage.save()
             self.refresh_layout()
         elif event == "Configure":
-            self._config.location = self._window.current_location(True)
-            self._config.size = self._window.size
+            self._config.location = self._window_holder.window.current_location(True)
+            self._config.size = self._window_holder.window.size
             self._configs_storage.save()
 
     def refresh_layout(self):
